e2f567af5a0e257f2e4c5f6d57058694864ff95a
Tony Lim <atomictheorist@gmail.com>
2014-12-03 01:15:17 -0800

    Change parts of Question object

--- app/src/main/java/com/awesomekids/android/quickcharades/Question.java
+++ app/src/main/java/com/awesomekids/android/quickcharades/Question.java
@@ -1,68 +1,68 @@
 package com.awesomekids.android.quickcharades;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Random;
 
 /**
  * Created by Joseph Park on 11/29/2014.
  */
 public class Question {
-    private Image image;
-    private char[] letterList;
+
+    // instead of using Image object, use imageID instead, category contains imageID, not the other way around
+//    private Image image;
+
+    private int mImageID;
+
+    public String getAnswer() {
+        return mAnswer;
+    }
+
+    private String mAnswer;
+    private ArrayList<Character> mRandomizedLetters;
     private Difficulty difficulty;
     private static int MAX_RAND = 15;
     private static int MAX_LETTER = 14;
 
-    public Question(){
-        image = new Image();
-        letterList = new char[MAX_LETTER];
-        difficulty = Difficulty.VOID;
+
+    // TODO: Implement difficulty
+    public Question(int imageID, String answer){
+//        this.difficulty = difficulty;
+        mRandomizedLetters = new ArrayList<Character>();
+        mImageID = imageID;
+        mAnswer = answer;
+        randomizeLetters();
     }
-    public Question(Image image, Difficulty difficulty){
-        this.image = new Image(image.getName(),image.getCategory(),image.getImageID());
-        this.difficulty = difficulty;
-        randomizeLetters(image.getName());
-    }
-    private void randomizeLetters(String imageName){
+
+    private void randomizeLetters() {
+        if (!mRandomizedLetters.isEmpty())
+            mRandomizedLetters.clear();
+
         Random Rand = new Random();
-        for(int i = 0; i < imageName.length(); i++)
-            letterList[i] = imageName.charAt(i);
-        for(int i = imageName.length(); i < MAX_LETTER; i++)
-            letterList[i] = (char)(Rand.nextInt(26)+'A');
-        for(int i = 0; i < MAX_RAND; i++){
+        for (int i = 0; i < mAnswer.length(); i++) {
+            mRandomizedLetters.add(mAnswer.charAt(i));
+        }
+        for (int i = mAnswer.length(); i < MAX_LETTER; i++)
+            mRandomizedLetters.add((char) (Rand.nextInt(26) + 'A'));
+        for (int i = 0; i < MAX_RAND; i++) {
             int a = Rand.nextInt(MAX_LETTER);
             int b = Rand.nextInt(MAX_LETTER);
-            swap(letterList,a,b);
+            Collections.swap(mRandomizedLetters, a, b); // using java collection
         }
-//        Random Rand = new Random(); //Test randomize, as well as find a better way to randomize
-//        letterList.clear();
-//        for(int i = 0; i < imageName.length(); i++)
-//            letterList.add(i,new Letter(imageName.charAt(i)));
-//        for(int i = imageName.length(); i < numOfLetters; i++)
-//            letterList.add(i,new Letter((char)(Rand.nextInt(26)+'0')));
-//        for(int i = 1; i <= RAND_CONSTANT; i++){
-//            int a = Rand.nextInt(numOfLetters);
-//            int b = Rand.nextInt(numOfLetters);
-//            //char temp = letterList.get(a).getValue();
-//            Letter temp = new Letter(letterList.get(a).getValue());
-//            letterList.set(a,letterList.get(b));
-//            letterList.set(b,temp);
-//        }
     }
-    private void swap(char[] cArray, int a, int b ){
-        char temp = cArray[a];
-        cArray[a] = cArray[b];
-        cArray[b] = temp;
+
+    // either pass in string or pass in charList
+    public boolean isCorrect(String answer){
+        return answer.equals(mAnswer);
     }
-    public Image getImage(){return image; }//return new Image(this.image.getName(),this.image.getCategory()); //Return a copy of the image or return the image?
-    public char[] getList(){return letterList; }//Same with this one ^
+
+    public int getImageID(){return mImageID; }//return new Image(this.image.getName(),this.image.getCategory()); //Return a copy of the image or return the image?
+
+    public ArrayList<Character> getRandomizedLetters() {
+        return mRandomizedLetters;
+    }
+
     public Difficulty getDifficulty(){return difficulty;}
-    public void setImage(Image image){
-        this.image.setName(image.getName());
-        this.image.setCategory(image.getCategory());
-        this.image.setImageID(image.getImageID());
-        randomizeLetters(image.getName());
-    }
     public void setDifficulty(Difficulty difficulty){this.difficulty = difficulty; }
-    public void rerollLetters(){randomizeLetters(image.getName());}
-    public boolean isCorrect(String answer){return answer.equals(image.getName());}
 }
